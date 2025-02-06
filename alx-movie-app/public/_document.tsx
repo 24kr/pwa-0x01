9bf9b33a023dc679daf3ef6a36a83485293ee43e
NGABO MBARAGA Eric <ericngabombaraga24@gmail.com>
2025-02-06 22:18:56 +0200

    All In

--- alx-movie-app/public/_document.tsx
+++ alx-movie-app/public/_document.tsx
@@ -1,26 +1,18 @@
+// pages/_document.js
+// eslint-disable-next-line @next/next/no-document-import-in-page
 import { Html, Head, Main, NextScript } from "next/document";
-import Document, { DocumentContext, DocumentInitialProps } from "next/document";
 
-class MyDocument extends Document {
-  static async getInitialProps(ctx: DocumentContext): Promise<DocumentInitialProps> {
-    const initialProps = await Document.getInitialProps(ctx);
-    return { ...initialProps };
-  }
-
-  render() {
-    return (
-      <Html lang="en">
-        <Head>
-          <link rel="manifest" href="/manifest.json" />
-          <meta name="theme-color" content="#0070f3" />
-        </Head>
-        <body className="antialiased">
-          <Main />
-          <NextScript />
-        </body>
-      </Html>
-    );
-  }
+export default function Document() {
+  return (
+    <Html lang="en">
+      <Head>
+        <link rel="manifest" href="/manifest.json" />
+        <meta name="theme-color" content="#0070f3" />
+      </Head>
+      <body className="antialiased">
+        <Main />
+        <NextScript />
+      </body>
+    </Html>
+  );
 }
-
-export default MyDocument;
